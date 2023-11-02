cc3d2483282d60fad48b1910c07be2638aa9b00a
JohnQ1981 <ikram1981@gmail.com>
2023-11-01 21:36:21 -0500

    FAST API

--- books.py
+++ books.py
@@ -1,15 +1,70 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
+class Book:
+    id: int
+    title: str
+    author: str
+    description: str
+    rating: int
+
+    def __init__(self, id, title, author, description, rating):
+        self.id = id
+        self.title = title
+        self.author = author
+        self.description = description
+        self.rating = rating
+
+
+class BookRequest(BaseModel):
+    id: int
+    title: str = Field(min_length=3)
+    author: str = Field(min_length=1)
+    description: str = Field(min_length=1, max_length=100)
+    rating: int = Field(gt=0, lt=6)
+    
+
+
+        
+
+
 BOOKS =[
-    {
-        "title": "John", "author": "Ikram", "category": "History"
-    }
+    Book(1, 'Computer Science Pro', 'codingwithJohn','A very nice book!', 5),
+    Book(2, 'Devops', 'codingwithJohn','Awesome book!', 5),
+    Book(3, 'Be Fast with FASTAPI', 'Learning FAST API','A great book!', 5),
+    Book(4, 'HP1', 'codingwithjohn','A very nice book!', 5),
+    Book(5, 'READ READ', 'codingwithjohn','A very nice book!', 5),
+    Book(6, 'Many Reads', 'FAST API','A very nice book!', 3),
+    Book(6, 'Book6', 'FAST API','A very nice book!', 5),
 ]
 
 
 @app.get("/books")
 async def read_all_books():
     return BOOKS
+
+
+# @app.post("/create_book")
+# async def create_book(book_request=Body()):
+#     BOOKS.append(book_request)
+
+@app.post("/create_book")
+async def create_book(book_request: BookRequest):
+    new_book =Book(**book_request.model_dump())
+    print(type(new_book))
+    BOOKS.append(find_book_id(new_book))
+
+
+def find_book_id(book: Book):
+    book.id = 1 if len(BOOKS) == 0 else BOOKS[-1].id + 1
+    # if len(BOOKS) > 0:
+    #     book.id = BOOKS[-1].id + 1
+    # else:
+    #     book.id = 1
+
+    return book
+
+
 
